e26fad5f91e7e369b14b85b89acacb90f86e990a
Николай <Ziberoff36@gmail.com>
2026-06-13 10:46:38 +0300

    Внедрение механики критического удара и промаха

--- battle.py
+++ battle.py
@@ -1,12 +1,22 @@
 from save import save
+import random
 def start_battle(player, enemy):
     print("Начинается бой!")
     print(f"{player.name} (HP: {player.health}) vs {enemy.name} (HP: {enemy.health})")
 def attack_battle(attacker, defender):
     damage = max(attacker.attack - defender.defense, 1)
-    defender.health -= damage
-    print(f"{attacker.name} наносит {damage} урона! У {defender.name} осталось {defender.health} HP")
-def defense_battle(attacker, defender):
+    random_attack = random.randint(1, 100)
+    if random_attack <= 20:
+        damage *= 2
+        print(f"{attacker.name} наносит критический удар!")
+        defender.health -= damage
+        print(f"{attacker.name} наносит {damage} урона! У {defender.name} осталось {defender.health} HP")
+    elif 20 < random_attack <= 30:
+        print(f"{attacker.name} промахнулся(-ась)!")
+    else:
+        defender.health -= damage
+        print(f"{attacker.name} наносит {damage} урона! У {defender.name} осталось {defender.health} HP")
+def defense_battle(defender, attacker):
     damage = max(attacker.attack - (defender.defense * 2), 1)
     defender.health -= damage
     print(f"{attacker.name} наносит {damage} урона! У {defender.name} осталось {defender.health} HP")
@@ -31,7 +41,7 @@ def battle(player, enemy):
             attack_battle(player, enemy)
             attack_battle(enemy, player)
         elif battle_choice == 2:
-            defense_battle(enemy, player)
+            defense_battle(player, enemy)
         elif battle_choice == 3:
             enemy.show_stats()
         elif battle_choice == 4:
